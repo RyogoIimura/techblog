169bbfb82f34be36ffeee428b8a00a6c74f4e827
kubo-work <kubowork0501@gmail.com>
2024-08-04 14:50:53 +0900

    blog ビューページ作成 (#15)
    OK

---
+++ app/blog/[id]/BlogContents.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import React, { FC } from "react";
+
+import "./BlogContents.modules.css";
+
+const BlogContents: FC<Record<string, never>> = React.memo(() => {
+  return (
+    <article className="blogArticle">
+      <header className="flex justify-center md:justify-between">
+        <h1>Blog Title</h1>
+        <div className="hidden md:block rounded-full w-24 h-24 overflow-hidden">
+          <Image
+            src="https://picsum.photos/200/300"
+            alt=""
+            width="96"
+            height="96"
+            style={{ width: "100%", height: "auto", objectFit: "contain" }}
+          />
+        </div>
+      </header>
+
+      <div className="mt-7">
+        <div>
+          <Image
+            src="https://picsum.photos/1200/700"
+            alt=""
+            width="1197"
+            height="618"
+            style={{ width: "100%", height: "auto" }}
+            priority
+          />
+        </div>
+
+        <div className="content mt-12">
+          <p>
+            ここに文章が入ります。
+            <br />
+            ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。
+          </p>
+          <p></p>
+          <p>
+            ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。ここに文章が入ります。
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+});
+
+BlogContents.displayName = "BlogContents";
+
+export default BlogContents;
