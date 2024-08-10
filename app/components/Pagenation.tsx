6d6299861e43094714e7f35c3c1a836330439e25
nobyn0312 <nobyn0312@gmail.com>
2024-08-10 20:38:02 +0900

    カードアイテムとしてコンポーネント切り出し

--- app/components/Pagenation.tsx
+++ app/components/Pagenation.tsx
@@ -23,11 +23,11 @@ export const Pagenation = () => {
   }, []);
 
   return (
-    <div className='flex justify-between mx-2'>
+    <div className='flex justify-between items-center mx-2'>
       <a>Prev</a>
       {visiblePages.map((num, index) => (
-        <div key={index} className='size-10  w-10 h-10'>
-          <a href={`/?page=${num}`} className="text-center p-2 bg-white rounded-full border-2  border-black block">{num}</a>
+        <div key={index} className=''>
+          <a href={`/?page=${num}`} className="w-14 h-14 text-center flex items-center justify-center bg-white rounded-full border-2  border-black">{num}</a>
         </div>
       ))}
           <a href="">Next</a>
