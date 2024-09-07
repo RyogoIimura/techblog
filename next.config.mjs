503cacd16e8e12fa76235e6667ec405fbdb910b9
nobyn0312 <nobyn0312@gmail.com>
2024-09-07 18:54:49 +0900

    imageを取得するために、configを変更

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['picsum.photos', 'enzejypzafacxwjdajnd.supabase.co', 'lh3.googleusercontent.com'],
-    },
+	images: {
+		domains: [
+			"picsum.photos",
+			'rfbshavdgwysigcizyjl.supabase.co',
+			"lh3.googleusercontent.com",
+		],
+	},
 };
 
 export default nextConfig;
